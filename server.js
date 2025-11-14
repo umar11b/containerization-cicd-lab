d67977d7e853b825c43f6940b7309ca5339186e2
umar11b <uzaman2018@gmail.com>
2025-11-14 10:52:21 -0500

    demo

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, Dockerized Node.js App!');
+app.get("/", (req, res) => {
+  res.send("Hello, Dockerized Node.js App! (demo)");
 });
 
 app.listen(PORT, () => {
-    console.log(`App running on http://localhost:${PORT}`);
+  console.log(`App running on http://localhost:${PORT}`);
 });
